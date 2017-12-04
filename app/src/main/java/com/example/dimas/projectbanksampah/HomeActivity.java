bd6c4384f456294013923efcb149b31058cbb190
Fazlur Rahman <fazlur97@gmail.com>
2017-12-04 22:31:47 +0700

    mbahana

--- app/src/main/java/com/example/dimas/projectbanksampah/HomeActivity.java
+++ app/src/main/java/com/example/dimas/projectbanksampah/HomeActivity.java
@@ -1,19 +1,32 @@
 package com.example.dimas.projectbanksampah;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 /**
  * Created by dimas on 11/28/2017.
  */
 
 public class HomeActivity extends Fragment{
+    private ImageView imageView;
+    private AnimationDrawable anim;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View v =inflater.inflate(R.layout.home_fragment, container, false);
+
+        imageView = (ImageView)v.findViewById(R.id.imageAnimation);
+        if(imageView == null) throw new AssertionError();
+        imageView.setBackgroundResource(R.drawable.animasi);
+        anim = (AnimationDrawable)imageView.getBackground();
+        anim.start();
+        return v;
     }
 }
